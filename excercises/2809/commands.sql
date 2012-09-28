1b8f276fedbd1cc5abda65b8f15786b4c932c496
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-28 15:10:34 +0200

    commands

--- excercises/2809/commands.sql
+++ excercises/2809/commands.sql
@@ -7,3 +7,9 @@ SELECT title, imdbRank as r, imdbVotes as v FROM danishMovies WHERE imdbRank =(S
 select year, count(*) from danishMovies group by year;
 
 select DISTINCT(10*floor(year/10)) as decade, count(*) as c from danishMovies GROUP BY decade;
+
+select director, min(year) as year FROM danishMovies group by director;
+
+select director, max(imdbRank) as r from danishMovies group by director order by r desc;
+
+
