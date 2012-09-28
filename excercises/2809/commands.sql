72f9f83b1dbc57151a0cbeba0cbd87cbc37b49f3
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-28 14:55:21 +0200

    more commands

--- excercises/2809/commands.sql
+++ excercises/2809/commands.sql
@@ -2,4 +2,8 @@ select title from danishMovies where imdbVotes>10000;
 
 select * from danishMovies where title like '%man%';
 
+SELECT title, imdbRank as r, imdbVotes as v FROM danishMovies WHERE imdbRank =(SELECT MAX(imdbRank) FROM danishMovies);
 
+select year, count(*) from danishMovies group by year;
+
+select DISTINCT(10*floor(year/10)) as decade, count(*) as c from danishMovies GROUP BY decade;
