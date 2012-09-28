6ef325e56cbfcc942340045ed702869c50bfc5c3
Christoffer Pedersen <stofferpedersen@gmail.com>
2012-09-28 14:38:44 +0200

    Added folders

--- handin1/IMDB.sql
+++ handin1/IMDB.sql
@@ -4,49 +4,46 @@ CREATE TABLE persons (
   birthDate DATE,
   deathDate DATE,
   description TEXT  
-)
+);
 
 CREATE TABLE genres (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL
-)
+);
 
 CREATE TABLE posTypes (
   id INT PRIMARY KEY AUTO_INCREMENT,  
   name VARCHAR(50)
-)
+);
 
 CREATE TABLE prodTypes (
   id INT PRIMARY KEY AUTO_INCREMENt,
   name VARCHAR(256)
-)
+);
 
 CREATE TABLE productions (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(256),
   typeId INT REFERENCES prodTypes(id),
   childOf INT REFERENCES productions(id)
-)
+);
 
-CREATE TABLE productionHasGenres
-(
-  production_id int,
-  genres_id int,
-  primary key (production_id, genres_id),
-  production_id references production(id),
-  genres_id references genres(id)
-)
+CREATE TABLE productionHasGenres(
+  production_id int references productions(id),
+  genres_id int references genres(id),
+  primary key (production_id, genres_id)
+);
 
 CREATE TABLE roles(
   id INT PRIMARY KEY,
   name VARCHAR(256)
-)
+);
 
 CREATE TABLE positionRoles(
   posId INT REFERENCES positions(id),
   rolId INT REFERENCES roles(id),
   PRIMARY KEY(posId, rolId)
-)
+);
 
 CREATE TABLE positions(
   startDate DATE,
@@ -55,11 +52,11 @@ CREATE TABLE positions(
   prodId int REFERENCES productions(id),
   posTypId int REFERENCES posTypes(id),
   PRIMARY KEY(persId, prodId, posTypId)
-)
+);
 
 
 CREATE TABLE releases(
   prodId INT NOT NULL REFERENCES productions(id),
   country VARCHAR(256),
   releaseDate DATE
-)
+);
