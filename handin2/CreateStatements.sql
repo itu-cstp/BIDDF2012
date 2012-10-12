f67f90458d179d9fd620582b10d455c2d201bd84
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-10-12 14:41:56 +0200

    Updated queries to create our schema in -> "CreateStatements.sql"

---
+++ handin2/CreateStatements.sql
@@ -0,0 +1,252 @@
+-- MySQL dump 10.13  Distrib 5.1.63, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: BIDD
+-- ------------------------------------------------------
+-- Server version	5.1.63-0ubuntu0.11.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `countries` (
+  `name` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `genres`
+--
+
+DROP TABLE IF EXISTS `genres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `hasCountry`
+--
+
+DROP TABLE IF EXISTS `hasCountry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hasCountry` (
+  `prodId` int(11) NOT NULL,
+  `countId` int(11) NOT NULL,
+  PRIMARY KEY (prodId,countId),
+  FOREIGN KEY (prodId) REFERENCES productions(id),
+  FOREIGN KEY (countId) REFERENCES countries(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `hasGenres`
+--
+
+DROP TABLE IF EXISTS `hasGenres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hasGenres` (
+  `prodId` int(11) NOT NULL,
+  `genreId` int(11) NOT NULL,
+  PRIMARY KEY (prodId,genreId),
+  FOREIGN KEY (prodId) REFERENCES productions(id),
+  FOREIGN KEY (genreId) REFERENCES genres(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `language`
+--
+
+DROP TABLE IF EXISTS `languages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `languages` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45),
+  PRIMARY KEY (Id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `persons`
+--
+
+DROP TABLE IF EXISTS `persons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `persons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `birthDate` date DEFAULT NULL,
+  `deathDate` date DEFAULT NULL,
+  `description` text,
+  `gender` varchar(1) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `posTypes`
+--
+
+DROP TABLE IF EXISTS `posTypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posTypes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `positions`
+--
+
+DROP TABLE IF EXISTS `positions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `positions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  `persId` int(11) NOT NULL DEFAULT '0',
+  `prodId` int(11) NOT NULL DEFAULT '0',
+  `posTypId` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id),
+  FOREIGN KEY (persId) REFERENCES persons(id),
+  FOREIGN KEY (prodId) REFERENCES productions(id),
+  FOREIGN KEY (posTypId) REFERENCES posTypes(id)
+) ENGINE=InnoDB  AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `positionRoles`
+--
+
+DROP TABLE IF EXISTS `positionRoles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `positionRoles` (
+  `posId` int(11) NOT NULL DEFAULT '0',
+  `rolId` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (posId,rolId),
+  FOREIGN KEY (posId) REFERENCES positions(id),
+  FOREIGN KEY (rolId) REFERENCES roles(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `productionHasGenres`
+--
+
+DROP TABLE IF EXISTS `productionHasGenres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productionHasGenres` (
+  `production_id` int(11) NOT NULL DEFAULT '0',
+  `genres_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (production_id,genres_id),
+  FOREIGN KEY (production_id) REFERENCES productions(id),
+  FOREIGN KEY (genres_id) REFERENCES genres(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `productions`
+--
+
+DROP TABLE IF EXISTS `productions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) DEFAULT NULL,
+  `childOf` int(11) DEFAULT NULL,
+  `releaseDate` date DEFAULT NULL,
+  `rating` double DEFAULT NULL,
+  `votes` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ratings`
+--
+
+DROP TABLE IF EXISTS `ratings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ratings` (
+  `user` varchar(20) DEFAULT NULL,
+  `movieId` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+--
+-- Table structure for table `hasLanguage`
+--
+
+DROP TABLE IF EXISTS `hasLanguage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hasLanguage` (
+  `prodId` int(11) NOT NULL,
+  `languageId` int(11) NOT NULL,
+  PRIMARY KEY (prodId,languageId),
+  FOREIGN KEY (prodId) REFERENCES productions(id),
+  FOREIGN KEY (languageId) REFERENCES languages(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- Dump completed on 2012-10-12 12:59:33
